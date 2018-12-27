91acad62d9f93466f0d560213e1e56d7bdaa120f
Yohan Hyunsung Yi <yohanhyunsungyi@gmail.com>
2018-12-27 00:28:18 -0600

    Initial Commit

---
+++ Speech Editor/Landing Page/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  AOT
+//
+//  Created by Hyun sung Yi on 11/23/18.
+//  Copyright © 2018 Yohan Hyunsung Yi. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+import MobileCoreServices
+
+class MainViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func videoPickerButtonAction(_ sender: Any) {
+        self.galleryTapped()
+    }
+    
+    // Handle the touch event on the gallery button
+    private func galleryTapped() {
+        let videoPickerController = UIImagePickerController()
+        videoPickerController.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) == false { return }
+        videoPickerController.allowsEditing = true
+        videoPickerController.sourceType = .photoLibrary
+        videoPickerController.mediaTypes = [kUTTypeMovie as String]
+        videoPickerController.modalPresentationStyle = .custom
+        self.present(videoPickerController, animated: true, completion: nil)
+    }
+
+    // After picking a video, we dismiss the picker view controller and present the editor view controller
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let videoURL = info[UIImagePickerController.InfoKey.referenceURL] as? URL
+        self.dismiss(animated: true, completion: nil)
+        
+        // We create a VideoEditorViewController to play video as well as for editing purpose
+        let MainEditorVC = MainEditorViewController()
+        MainEditorVC.videoURL = videoURL
+        MainEditorVC.videoAsset = AVURLAsset(url: videoURL!)
+        self.present(MainEditorVC, animated: true, completion: nil)
+    }
+}
+
---
+++ Speech Editor/MainEditor Scene/AVAssetExtension.swift
@@ -0,0 +1,55 @@
+//
+//  AVAssetExtension.swift
+//  Speech Editor
+//
+//  Created by Hyun sung Yi on 12/26/18.
+//  Copyright © 2018 Yohan Hyunsung Yi. All rights reserved.
+//
+
+import AVFoundation
+
+extension AVAsset {
+    
+    func writeAudioTrackToURL(_ url: URL, completion: @escaping (Bool, Error?) -> ()) {
+        do {
+            let audioAsset = try self.audioAsset()
+            audioAsset.writeToURL(url, completion: completion)
+        } catch (let error as NSError){
+            completion(false, error)
+        }
+    }
+    
+    func writeToURL(_ url: URL, completion: @escaping (Bool, Error?) -> ()) {
+        
+        guard let exportSession = AVAssetExportSession(asset: self, presetName: AVAssetExportPresetAppleM4A) else {
+            completion(false, nil)
+            return
+        }
+        
+        exportSession.outputFileType = .m4a
+        exportSession.outputURL      = url
+        
+        exportSession.exportAsynchronously {
+            switch exportSession.status {
+            case .completed:
+                completion(true, nil)
+            case .unknown, .waiting, .exporting, .failed, .cancelled:
+                completion(false, nil)
+            }
+        }
+    }
+    
+    func audioAsset() throws -> AVAsset {
+        
+        let composition = AVMutableComposition()
+        let audioTracks = tracks(withMediaType: .audio)
+        
+        for track in audioTracks {
+            
+            let compositionTrack = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+            try compositionTrack?.insertTimeRange(track.timeRange, of: track, at: track.timeRange.start)
+            compositionTrack?.preferredTransform = track.preferredTransform
+        }
+        return composition
+    }
+}
